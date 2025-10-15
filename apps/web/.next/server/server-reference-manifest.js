d4826b9530e2eff47e3c3e45abbc8683408e5015
Banti4750 <bantikumar6203818460@gmail.com>
2025-10-15 22:40:46 +0530

    dd

--- apps/web/.next/server/server-reference-manifest.js
+++ apps/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"z7Cape51UgSisvBaquEyeC4uyMT4Dkjq9cni/4IMgSI=\"\n}"
